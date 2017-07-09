fa75dfc07b2e1a9d4372a18d2fbcff919a94d4e1
Pascal Anglade <pascal.nglade@gmail.com>
2017-07-09 16:16:35 -0400

    fix a bug from

--- player.js
+++ player.js
@@ -13,6 +13,54 @@ class Player{
         this.reset();
     }
 
+    createPiece(type){
+    
+        switch(type){
+            case 'T':
+                return [
+                    [0, 0, 0],
+                    [1, 1, 1],
+                    [0, 1, 0]
+                ];
+            case 'O':
+                return [
+                    [2, 2],
+                    [2, 2]
+                ];
+            case 'L':
+                return [
+                    [0, 3, 0],
+                    [0, 3, 0],
+                    [0, 3, 3]
+                ];
+            case 'J':
+                return [
+                    [0, 4, 0],
+                    [0, 4, 0],
+                    [4, 4, 0]
+                ];
+            case 'I':
+                return [
+                    [0, 5, 0, 0],
+                    [0, 5, 0, 0],
+                    [0, 5, 0, 0],
+                    [0, 5, 0, 0]
+                ];
+            case 'S':
+                return [
+                    [0, 6, 6],
+                    [6, 6, 0],
+                    [0, 0, 0]
+                ];
+            case 'Z':
+            return [
+                    [7, 7, 0],
+                    [0, 7, 7],
+                    [0, 0, 0]
+                ]; 
+        }
+    }
+
     drop(){
         this.pos.y++;
         if(this.arena.collide(this)){
@@ -34,7 +82,7 @@ class Player{
 
     reset(){
         const pieces = 'ILJOTSZ';
-        this.matrix = createPiece(pieces[pieces.length * Math.random() | 0]); //Math.random() | 0 <- floors the number
+        this.matrix = this.createPiece(pieces[pieces.length * Math.random() | 0]); //Math.random() | 0 <- floors the number
         this.pos.y = 0;
         this.pos.x = (this.arena.matrix[0].length/2 | 0)  - (this.matrix[0].length/2 | 0);
         if(this.arena.collide(this)){
